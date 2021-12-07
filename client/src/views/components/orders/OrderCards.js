6b9f9f9a4b08b4e701e68a5dd1840924495df397
sumit245 <sumitranjan245@gmail.com>
2021-12-07 11:10:40 +0530

    Orders

--- client/src/views/components/orders/OrderCards.js
+++ client/src/views/components/orders/OrderCards.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function OrderCards({ total }) {
+export default function OrderCards({ total,pending,started,completed,rejected,cancelled }) {
   return (
     <div className="row">
       <div className="col-lg-3">
@@ -18,6 +18,23 @@ export default function OrderCards({ total }) {
           </div>
         </div>
       </div>
+      <div className="col-lg-3">
+        <div className="ibox">
+          <div className="ibox-title">
+            <span className="label label-warning float-right">Monthly</span>
+            <h5>Pending</h5>
+          </div>
+          <div className="ibox-content">
+            <h1 className="no-margins">{pending}</h1>
+            <div className="stat-percent font-bold text-warning">
+              0% <i className="fa fa-level-down" />
+            </div>
+            <small>Orders placed by user but not accepted</small>
+          </div>
+        </div>
+      
+      
+      </div>
 
       <div className="col-lg-3">
         <div className="ibox">
@@ -26,7 +43,7 @@ export default function OrderCards({ total }) {
             <h5>In Progress</h5>
           </div>
           <div className="ibox-content">
-            <h1 className="no-margins">0</h1>
+            <h1 className="no-margins">{started}</h1>
             <div className="stat-percent font-bold text-warning">
               0% <i className="fa fa-level-up" />
             </div>
@@ -42,7 +59,7 @@ export default function OrderCards({ total }) {
             <h5>Completed</h5>
           </div>
           <div className="ibox-content">
-            <h1 className="no-margins">0</h1>
+            <h1 className="no-margins">{completed}</h1>
             <div className="stat-percent font-bold text-success">
               0% <i className="fa fa-level-up" />
             </div>
@@ -58,14 +75,34 @@ export default function OrderCards({ total }) {
             <h5>Cancelled</h5>
           </div>
           <div className="ibox-content">
-            <h1 className="no-margins">0</h1>
+            <h1 className="no-margins">{cancelled}</h1>
             <div className="stat-percent font-bold text-danger">
               0% <i className="fa fa-level-down" />
             </div>
             <small>Orders cancelled by user</small>
           </div>
         </div>
+      
+      
       </div>
+      <div className="col-lg-3">
+        <div className="ibox">
+          <div className="ibox-title">
+            <span className="label label-danger float-right">Monthly</span>
+            <h5>Rejected</h5>
+          </div>
+          <div className="ibox-content">
+            <h1 className="no-margins">{rejected}</h1>
+            <div className="stat-percent font-bold text-danger">
+              0% <i className="fa fa-level-down" />
+            </div>
+            <small>Orders rejected by chef</small>
+          </div>
+        </div>
+      
+      
+      </div>
+      
     </div>
   );
 }
--- client/src/views/screens/Orders.js
+++ client/src/views/screens/Orders.js
@@ -6,17 +6,39 @@ import OrderCards from "../components/orders/OrderCards";
 
 export default function Orders() {
   const [orders, setOrders] = useState([]);
+  const [completed, setCompleted] = useState(0);
+  const [rejected, setrejected] = useState(0);
+  const [pending, setPending] = useState(0);
+  const [started, setstarted] = useState(0);
+  const [cancelled, setCancelled] = useState(0);
+  const setOrderCounts = async () => {
+    const response = await axios.get("/api/orders");
+    const order = await response.data;
+    setOrders(order);
+    let accepted = order.filter((item) => item.status === "accepted");
+    let rejectedorders = order.filter((item) => item.status === "rejected");
+    let startedorders = order.filter((item) => item.status === "started");
+    let cancelledorders = order.filter((item) => item.status === "cancelled");
+    let completedorders = order.filter((item) => item.status === "completed");
+    setstarted(startedorders.length);
+    setPending(accepted.length);
+    setrejected(rejectedorders.length);
+    setCancelled(cancelledorders.length);
+    setCompleted(completedorders.length)
+  };
   useEffect(() => {
-    axios
-      .get("/api/orders")
-      .then((res) => {
-        setOrders(res.data);
-      })
-      .catch((err) => console.error(err));
+    setOrderCounts();
   }, []);
   return (
     <div className="wrapper wrapper-content">
-      <OrderCards total={orders.length} />
+      <OrderCards
+        total={orders.length}
+        completed={completed}
+        started={started}
+        pending={pending}
+        rejected={rejected}
+        cancelled={cancelled}
+      />
       <Table title="Orders" data={orders} flag={true} columns={orderColumns} />
     </div>
   );
