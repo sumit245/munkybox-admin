907075aca55c7f4fbff64083b2277e4403966702
sumit245 <sumitranjan245@gmail.com>
2021-12-09 13:55:01 +0530

    Message

--- api/orders.js
+++ api/orders.js
@@ -89,7 +89,7 @@ router.route("/dashboard/:restaurant_name").get(async (req, res) => {
   let restaurant = req.params.restaurant_name;
   const response = await Order.find({
     restaurant: restaurant,
-    $or: [{ status: "started" }, { status: "accepted" }, { status: "cancelled" }],
+    $or: [{ status: "started" }, { status: "accepted" }, { status: "cancelled" },{status:"completed"}],
   });
 
   const twoOrders = await response.filter((item) => item.plan === "twoPlan");
