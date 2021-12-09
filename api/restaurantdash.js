d8c269a9ca11cb8aa926584541050c84a93f1589
sumit245 <sumitranjan245@gmail.com>
2021-12-10 03:17:10 +0530

    message

--- api/restaurantdash.js
+++ api/restaurantdash.js
@@ -3,15 +3,32 @@ const router = express.Router();
 const NewRestaurant = require("../models/newrest.model");
 const Orders = require("../models/orders.model");
 const Users = require("../models/users.model");
+const RestaurantDashboard = require("../models/restaurant_dashboard.model");
 
 router.route("/getusertypesbyrestaurant/:restaurant").get(async (req, res) => {
   const { restaurant } = req.params;
   const myOrders = await Orders.find({ restaurant: restaurant });
+  const myDashboard = await RestaurantDashboard.findOne({
+    restaurant: restaurant,
+  });
   const userids = myOrders.map((item) => item.user_id);
   let uniq = [...new Set(userids)];
+  const countOccurrences = (arr, val) =>
+    arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+  let x = uniq.map((item) => countOccurrences(userids, item));
+  newUse = myDashboard.newusers;
+  repeat = myDashboard.repeatedUsers;
+  x.forEach((element) => {
+    if (element !== 1) {
+      repeat += 1;
+    } else {
+      newUse += 1;
+    }
+  });
   res.json({
     newusers: uniq.length,
-    repeatedUsers: userids.length - uniq.length,
+    repeatedUsers: repeat,
+    more: x,
   });
 });
 
