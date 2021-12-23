056c0797f741284afdf2fc57a4ef24edb60fd7a3
sumit245 <sumitranjan245@gmail.com>
2021-12-23 14:31:57 +0530

    Mesaf

--- models/restaurant_dashboard.model.js
+++ models/restaurant_dashboard.model.js
@@ -46,6 +46,28 @@ let RestaurantDashboard = new Schema({
       totalUsed: { type: String },
     },
   ],
+  banners: [
+    {
+      promo_id: String,
+      restaurant_id: String,
+      plan_name: String,
+      rpc: String,
+      duration: String,
+      status: { type: String, default: "Inactive" },
+      discount_type: String,
+      meal_plan: String,
+      discount: String,
+      promo_code: String,
+      start_date: String,
+      end_date: String,
+      clicks: { type: Number, default: 0 },
+      due: { type: Number, default: 0 },
+      paid: { type: Number, default: 0 },
+      users: { type: Number, default: 0 },
+      orders: { type: Number, default: 0 },
+      revenue: { type: Number, default: 0 },
+    },
+  ],
 });
 
 module.exports = mongoose.model("RestaurantDashboard", RestaurantDashboard);
