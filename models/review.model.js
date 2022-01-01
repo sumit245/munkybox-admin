255f8f14a2f1611fc7fe984f85d2f355715dd7d7
sumit245 <sumitranjan245@gmail.com>
2022-01-01 07:51:04 +0530

    Message

--- models/review.model.js
+++ models/review.model.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = new Schema({
   user_id: String,
+  user_name: String,
   restaurant_id: String,
   role: String,
   order_id: String,
-  order_time:String,
+  order_time: String,
   delivered_on: String,
   plan_name: String,
   base_price: String,
@@ -15,6 +16,7 @@ const Review = new Schema({
   comments: [
     {
       role: String,
+      restaurant_name: String,
       body: String,
       commented_at: { type: String, default: Date.now().toLocaleString() },
     },
