877ec682dad8e4f5888576a7f664768c418d2c8c
sumit245 <sumitranjan245@gmail.com>
2022-01-26 22:50:24 +0530

    Message

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const connection = process.env.MONGO_URL;
+const connection = process.env.MONGODB_URL;
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.warn("Database Connected Successfully"))
     .catch(err => console.error(err));
