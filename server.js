91f6475071e2074d6578b86fde8f35838e0631a1
sumit245 <sumitranjan245@gmail.com>
2022-03-28 03:30:32 +0530

    message

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use("/api/review", review);
 app.use("/api/stripe", stripeintent)
 
 app.use(express.static(path.join(__dirname, "./build/")));
-app.get("/**", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./build/"));
 });
 
